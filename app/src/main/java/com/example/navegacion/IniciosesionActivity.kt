d94ff87193da1e7c0457e9a52c3388f275a13290
Valencaicedo <valentina.caicedos@hotmail.com>
2025-04-24 11:59:09 -0500

    Interacción botones

--- app/src/main/java/com/example/navegacion/IniciosesionActivity.kt
+++ app/src/main/java/com/example/navegacion/IniciosesionActivity.kt
@@ -1,6 +1,8 @@
 package com.example.navegacion
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,19 @@ class IniciosesionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_iniciosesion)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // botón de regreso
+        val btnRegresar = findViewById<ImageView>(R.id.btn_regresaramain)
+        btnRegresar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
