185fcba15657328c6c394b65391b5353ff9e17e9
Valencaicedo <valentina.caicedos@hotmail.com>
2025-04-24 13:00:24 -0500

    Interacción botones

--- app/src/main/java/com/example/navegacion/IniciosesionActivity.kt
+++ app/src/main/java/com/example/navegacion/IniciosesionActivity.kt
@@ -8,7 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+//Tania Valentina Caicedo y Laura Castañeda
 class IniciosesionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
